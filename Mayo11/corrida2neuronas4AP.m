fae628e1337df76b0a5509da6ca639f64779b7bf
davoine <federico.davoine@gmail.com>
2011-05-22 23:50:28 -0300

    Script corrida masiva modelo 2 neuronas con 4AP (corrida2neuronas4AP.m) corregido

--- Mayo11/corrida2neuronas4AP.m
+++ Mayo11/corrida2neuronas4AP.m
@@ -5,14 +5,8 @@ clc
 g4AParray = [0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9]; % en pS
 for w = 1:length(g4AParray)
 n = 1;
-<<<<<<< HEAD
-m = 1;
-
-g4AP = 1e7;
-=======
 g4AP = g4AParray(w);
 I_iny = 5e5; % en pA
->>>>>>> 2c9e60a456db8215a4433d3aecf2f163b91d499c
 for k = 1:1:10
     transferencia(w,n)=modelo2neuronas4AP(k,50,I_iny,1e-2,g4AP);
     frecuencias(w,n) = k;
@@ -31,15 +25,8 @@ for k = 100:50:1000
     n = n + 1
 end
 
-<<<<<<< HEAD
-figure(5)
-xlabel('frecuencia (Hz)')
-semilogx(frecuencias,transferencia)
-=======
 end
 % figure(5)
 % semilogx(frecuencias,transferencia);
 
 save
-
->>>>>>> 2c9e60a456db8215a4433d3aecf2f163b91d499c
